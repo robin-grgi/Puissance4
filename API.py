7dc41875475b91b4f6ae9cf23f259cf9683e0100
Michael Mc Carthy <59956094+Themccarthy@users.noreply.github.com>
2023-04-24 22:45:36 +0200

    finish API + algo fixes and opti

--- API.py
+++ API.py
@@ -4,17 +4,19 @@ import functions
 
 app = FastAPI()
 
+
 def handle_error(statusCode, message):
     raise HTTPException(status_code=statusCode, detail=message)
 
 
 def handle_invalid_board(board):
     board_valid, board_status = functions.is_board_valid(board)
-    if not board_valid :
+    if not board_valid:
         handle_error(400, board_status)
 
-def handle_game_over(board):
-    game_over, status = functions.is_game_over(board)
+
+def handle_game_over(board, player):
+    game_over, status = functions.is_game_over(board, player)
     if game_over and status == "tie":
         handle_error(422, "Board full")
     elif game_over and status == "player":
@@ -22,14 +24,15 @@ def handle_game_over(board):
     elif game_over and status == "opponent":
         handle_error(422, "machine won")
 
+
 @app.get("/move")
 def get_best_move(b: str):
     handle_invalid_board(b)
-    handle_game_over(b)
     board = functions.create_board(b)
+    player = 1
+    handle_game_over(board, player)
     depth = 4
     alpha = float('-inf')
     beta = float('inf')
-    player = 1
-    best_move, best_score = functions.get_best_move(board, depth, alpha, beta, player, player)
-    return best_move
+    best_move, best_score = functions.get_best_move(board, depth, alpha, beta, True, player, player)
+    return best_move + 1
--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import numpy as np
 import re
 
+
 def get_possible_moves(board):
     return [col for col in range(7) if (board[:, col] == 0).any()]
 
@@ -13,17 +14,18 @@ def is_board_valid(board_string):
         if char not in ['m', 'h', '0']:
             return False, "Invalid character in board"
 
+    if board_string.count("h") != (board_string.count("m") + 1):
+        return False, "Invalid game state"
+
     for col in range(7):
         start = col * 6
 
         end = start + 6
-        if re.match("@*0[mh]",board_string[start:end]):
+        if re.search("@*0[mh]", board_string[start:end]):
             return False, "Invalid board configuration"
 
-    if board_string.find("h") != board_string.find("m") + 1:
-        return False, "Invalid game state"
-
     return True, "Valid board"
+
 
 def create_board(board_string):
     board = np.zeros((6, 7), dtype=int)
@@ -40,8 +42,9 @@ def create_board(board_string):
 
 def get_aligned_checkers(board, player):
     player_aligned = np.diff(np.concatenate(([False], board == player, [False])).astype(int))
+    opponent_aligned = np.diff(np.concatenate(([False], board == -player, [False])).astype(int))
     return np.flatnonzero(player_aligned == -1) - np.flatnonzero(player_aligned == 1), \
-           np.flatnonzero(player_aligned == 1) - np.flatnonzero(player_aligned == -1)
+           np.flatnonzero(opponent_aligned == -1) - np.flatnonzero(opponent_aligned == 1)
 
 
 def update_score(board, player, scores, scores_array, diagonal=False):
@@ -71,20 +74,21 @@ def evaluate_board(board, player, depth):
     # diagonal check (negative slope)
     scores = update_score(np.fliplr(board), player, scores, scores_array, diagonal=True)
 
-    return (scores[1] - scores[0]) * depth
+    return (scores[0] - scores[1]) * depth
 
 
-def get_best_move(board, depth, alpha, beta, maximizing_player, current_player):
+def get_best_move(board, depth, alpha, beta, maximizing_player, study_player, current_player):
     # check if game is over or depth limit reached
-    if depth == 0 or is_game_over(board):
-        return None, evaluate_board(board, maximizing_player, depth + 1)
+    status, message = is_game_over(board, current_player)
+    if depth == 0 or status:
+        return None, evaluate_board(board, study_player, depth + 1)
 
     # get possible moves
     possible_moves = get_possible_moves(board)
 
     # if there are no possible moves, return the evaluation of the current board
     if len(possible_moves) == 0:
-        return None, evaluate_board(board, maximizing_player, depth + 1)
+        return None, evaluate_board(board, study_player, depth + 1)
 
     # initialize best move and score
     if maximizing_player:
@@ -94,7 +98,9 @@ def get_best_move(board, depth, alpha, beta, maximizing_player, current_player):
         best_move = possible_moves[0]
         best_score = float('inf')
 
-    score = evaluate_board(board, maximizing_player, depth + 1)
+    # if current player is opponent, simulate his best move
+    if current_player != study_player:
+        board = play_opponent(board, possible_moves, current_player, depth)
 
     # iterate through possible moves
     for move in possible_moves:
@@ -103,7 +109,7 @@ def get_best_move(board, depth, alpha, beta, maximizing_player, current_player):
         play_move(new_board, current_player, move)
 
         # recursively get the best score for the next player
-        score += get_best_move(new_board, depth - 1, alpha, beta, maximizing_player, -current_player)[1]
+        score = get_best_move(new_board, depth - 1, alpha, beta, not maximizing_player, study_player, -current_player)[1]
 
         # update alpha and beta values
         if maximizing_player:
@@ -124,37 +130,36 @@ def get_best_move(board, depth, alpha, beta, maximizing_player, current_player):
     return best_move, best_score
 
 
-def is_game_over(board):
+def is_game_over(board, player):
     # horizontal check
     for row in range(len(board)):
-        play, opp = get_aligned_checkers(board[row], 1)
+        play, opp = get_aligned_checkers(board[row], player)
         if 4 in play:
             return True, "player"
         elif 4 in opp:
             return True, "opponent"
 
-
     # vertical check
     transposed_board = board.transpose()
     for row in range(len(board)):
-        play, opp = get_aligned_checkers(transposed_board[row], 1)
-        if 4 in play :
+        play, opp = get_aligned_checkers(transposed_board[row], player)
+        if 4 in play:
             return True, "player"
         elif 4 in opp:
             return True, "opponent"
 
     # diagonal check (positive slope)
     for row in range(-(len(board) - 1), len(board) + 1):
-        play, opp = get_aligned_checkers(board.diagonal(row), 1)
-        if 4 in play :
+        play, opp = get_aligned_checkers(board.diagonal(row), player)
+        if 4 in play:
             return True, "player"
         elif 4 in opp:
             return True, "opponent"
 
     # diagonal check (negative slope)
     for row in range(-(len(board) - 1), len(board) + 1):
-        play, opp = get_aligned_checkers(np.fliplr(board).diagonal(row), 1)
-        if 4 in play :
+        play, opp = get_aligned_checkers(np.fliplr(board).diagonal(row), player)
+        if 4 in play:
             return True, "player"
         elif 4 in opp:
             return True, "opponent"
@@ -173,3 +178,16 @@ def play_move(board, player, col):
             board[row][col] = player
             return board
     return board  # column is full, return original board
+
+
+def play_opponent(board, possible_moves, current_player, depth):
+    best_opp_board = np.copy(board)
+    best_opp_score = 0
+    for move in possible_moves:
+        new_board = np.copy(board)
+        play_move(new_board, current_player, move)
+        tmp_score = evaluate_board(new_board, current_player, depth)
+        if tmp_score > best_opp_score:
+            best_opp_score = tmp_score
+            best_opp_board = np.copy(new_board)
+    return best_opp_board
